dc4019babd98465005cf90fa69c4bdc62d03be48
sungminpark <devmin0714@gmail.com>
2024-02-29 16:04:22 +0900

    axios 설정

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,20 +3,6 @@ import axios from 'axios';
 
 
 function App() {
-    const username = 'min1'
-    const email = 'min1@min.com'
-    const password = 'min123456'
-    const password2 = 'min123456'
-
-    useEffect(() => {
-        axios.post('http://127.0.0.1:8000/api/account/register/', {
-                username,
-                email,
-                password,
-                password2,
-            });
-    }, [])
-
     return (
         <>
             <div>test</div>
